3aecb4a0970cd884942046a29af6d541e9e3d2dc
Khai <krobe114@charlotte.edu>
2026-02-08 21:14:39 -0500

    Add logic for make_sandwich

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -81,5 +81,7 @@ class SandwichMachine:
     def make_sandwich(self, sandwich_size, order_ingredients):
         """Deduct the required ingredients from the resources.
            Hint: no output"""
-
+        for item, amount in order_ingredients.items():
+            self.machine_resources[item] -= amount
+        print(f"{sandwich_size} sandwich is ready. Bon appetit!")
 ### Make an instance of SandwichMachine class and write the rest of the codes ###
