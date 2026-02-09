fa278cc8b2ad41a437d8eaa3facadb1c5db82230
Khai <krobe114@charlotte.edu>
2026-02-08 20:39:59 -0500

    Added logic for check_resources function.

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -45,6 +45,11 @@ class SandwichMachine:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
+        for item, amount in ingredients.items():
+            if self.machine_resources[item] < amount:
+                print(f"Sorry there is not enough {item}.")
+                return False
+        return True
 
     def process_coins(self):
         """Returns the total calculated from coins inserted.
